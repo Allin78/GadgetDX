3cf5937e1b43105d09864d0b5ba2685ad4813d35
cristian <c.alfano@outlook.it>
2020-07-28 02:05:56 +0200

    Extending Mi Band 5 support! Every display item can be showed on the smartband now.

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/miband5/MiBand5Coordinator.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/huami/miband5/MiBand5Coordinator.java
@@ -88,7 +88,7 @@ public class MiBand5Coordinator extends HuamiCoordinator {
     @Override
     public int[] getSupportedDeviceSpecificSettings(GBDevice device) {
         return new int[]{
-                R.xml.devicesettings_miband3,
+                R.xml.devicesettings_miband5,
                 R.xml.devicesettings_wearlocation,
                 R.xml.devicesettings_custom_emoji_font,
                 R.xml.devicesettings_timeformat,
