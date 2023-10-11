5f5b1d070bdcdce8fa1fc5e9cea110f35fca8c0c
Ganblejs <geatre+codeberg@gmail.com>
2023-10-11 23:13:15 +0200

    Bangle.js: undo trying to filter musicinfo/state

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/banglejs/BangleJSDeviceSupport.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/banglejs/BangleJSDeviceSupport.java
@@ -1296,8 +1296,6 @@ public class BangleJSDeviceSupport extends AbstractBTLEDeviceSupport {
         }
     }
 
-    private JSONObject prevMusicStateObject;
-
     @Override
     public void onSetMusicState(MusicStateSpec stateSpec) {
         try {
@@ -1311,16 +1309,11 @@ public class BangleJSDeviceSupport extends AbstractBTLEDeviceSupport {
             o.put("position", stateSpec.position);
             o.put("shuffle", stateSpec.shuffle);
             o.put("repeat", stateSpec.repeat);
-            LOG.info("prevMusicState 1: " + String.valueOf(prevMusicStateObject));
-            if (true || !String.valueOf(o).equals(String.valueOf(prevMusicStateObject))) uartTxJSON("onSetMusicState", o);
-            prevMusicStateObject = o;
-            LOG.info("prevMusicState 2: " + String.valueOf(prevMusicStateObject));
+            uartTxJSON("onSetMusicState", o);
         } catch (JSONException e) {
             LOG.info("JSONException: " + e.getLocalizedMessage());
         }
     }
-
-    private JSONObject prevMusicInfoObject;
 
     @Override
     public void onSetMusicInfo(MusicSpec musicSpec) {
@@ -1333,10 +1326,7 @@ public class BangleJSDeviceSupport extends AbstractBTLEDeviceSupport {
             o.put("dur", musicSpec.duration);
             o.put("c", musicSpec.trackCount);
             o.put("n", musicSpec.trackNr);
-            LOG.info("prevMusicINfo 1: " + String.valueOf(prevMusicInfoObject));
-            if (true || !String.valueOf(o).equals(String.valueOf(prevMusicInfoObject))) uartTxJSON("onSetMusicInfo", o);
-            prevMusicInfoObject = o;
-            LOG.info("prevMusicINfo 2: " + String.valueOf(prevMusicInfoObject));
+            uartTxJSON("onSetMusicInfo", o);
         } catch (JSONException e) {
             LOG.info("JSONException: " + e.getLocalizedMessage());
         }
