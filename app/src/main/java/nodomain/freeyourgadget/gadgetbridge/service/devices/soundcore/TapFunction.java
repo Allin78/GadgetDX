5fa9fffb44570b2262c4b4152204d9e889ce8d75
ahormann <ahormann@gmx.net>
2024-05-07 12:02:06 +0200

    Soundcore: Fixed Values

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/soundcore/TapFunction.java
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/service/devices/soundcore/TapFunction.java
@@ -1,9 +1,9 @@
 package nodomain.freeyourgadget.gadgetbridge.service.devices.soundcore;
 enum TapFunction {
-    VOLUMEDOWN(1),
-    VOLUMEUP(0),
-    NEXT( 3),
-    PREVIOUS(2),
+    VOLUME_DOWN(1),
+    VOLUME_UP(0),
+    MEDIA_NEXT( 3),
+    MEDIA_PREV(2),
     PLAYPAUSE(6),
     VOICE_ASSISTANT(5),
     AMBIENT_SOUND_CONTROL(4)
